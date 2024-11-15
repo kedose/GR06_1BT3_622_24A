04b30ed89f185c0fa9e88f143b863273cd2973c3
Damarys06 <andrea.onacalahorrano@epn.edu.ec>
2024-11-14 22:08:03 -0500

    Update InicioController.java

--- src/main/java/com/poliweb/controladores/InicioController.java
+++ src/main/java/com/poliweb/controladores/InicioController.java
@@ -10,9 +10,11 @@ import java.io.IOException;
 
 @WebServlet("/Inicio")
 public class InicioController extends HttpServlet {
-    // Sobrescribe el método doGet para manejar solicitudes GET al servlet  
+    // Sobrescribe el método doGet para manejar solicitudes GET al servlet
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Redirige la solicitud y la respuesta a la página bienvenida.jsp
         request.getRequestDispatcher("/bienvenida.jsp").forward(request, response);
     }
 }

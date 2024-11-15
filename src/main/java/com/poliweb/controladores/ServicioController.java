3df24fa3d5d1b455b8e4a7de067a118f2e8d0f12
Damarys06 <andrea.onacalahorrano@epn.edu.ec>
2024-11-14 22:15:07 -0500

    Update ServicioController.java

--- src/main/java/com/poliweb/controladores/ServicioController.java
+++ src/main/java/com/poliweb/controladores/ServicioController.java
@@ -9,8 +9,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 @WebServlet("/Servicios")
 public class ServicioController extends HttpServlet {
+    // Sobrescribe el método doGet para manejar solicitudes GET al servlet
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Redirige la solicitud y la respuesta a la página index.jsp
+        // para mostrar la interfaz principal o de inicio
         request.getRequestDispatcher("/index.jsp").forward(request, response);
     }
 }
